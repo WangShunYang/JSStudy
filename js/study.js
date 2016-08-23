25fe1849ec47302f125400ca30c55930ca0c1c11
顺扬 <swang@evolveconsulting.com.hk>
2016-08-23 18:13:42 +0800

    1.002

--- js/study.js
+++ js/study.js
@@ -1,1 +1,52 @@
-alert('开始学习JS');
+'use strict'
+alert('hello,world');
+//var x = 1;
+//'hello,world';
+//var x = 1;
+//var y = 2;
+//if(2 > 1){
+//	x = 1;
+//	y = 2;
+//	z = 3;
+//}
+////alert(x);
+//alert(y);
+//alert(z);
+//var a = 1+2;
+//var b = (1 + 2 +3 + 4 + 5)/2;
+//alert(a);
+//alert(b);
+//var arr = [1,2,3.14,'hello,world'];
+////for (var i = 0; i<arr.Counter();i++){
+//	alert(arr);
+////}
+// 对象
+var person = {
+	name:'bob',
+	age:20,
+	tags:['js','web','mobile'],
+	city:'shanghai',
+	hasCar:true,
+	zipcode:null	
+};
+
+alert(person);
+alert(person.name);
+// 变量
+//var a; // 申明了变量a ,此时a的值为undefined
+//var $b = 1;// 申明了变量$b ,同时给$b赋值,此时$b的值为1
+//var s_007 = '007'; // s_007 是一个字符串
+//var answer = true; // answer 是一个布尔值 值true
+//var t = null; // t的值是null
+
+//
+//var a = 123; // a 的值是整数123
+// a = 'abc'; // a 变为字符串
+// alert(a);
+  // int b = 123;
+// b = 'abc'; // 错误: 不能把字符串赋给整形变量
+ //  alert(b);//
+  var name = '小明';
+  var age = 20;
+  var message = '你好,' + name + ',你今年' + age + '岁了!';
+  alert(message);
